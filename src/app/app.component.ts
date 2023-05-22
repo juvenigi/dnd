aaf99241bfca7afa0f4b7313ad5ccc1dd0989c2b
Dmytro <juvenigi@gmail.com>
2023-05-22 06:29:17 +0200

    add ngx-social-login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Component, ElementRef, inject, ViewChild} from '@angular/core';
 import {Die} from './model/enum/die.enum';
 import {DiceService} from './service/dice.service';
 import {forkJoin, lastValueFrom, reduce} from 'rxjs';
+import {Environment} from "../environments/environment";
+import {GoogleLoginProvider, SocialAuthService} from "@abacritt/angularx-social-login";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -11,15 +14,109 @@ import {forkJoin, lastValueFrom, reduce} from 'rxjs';
 
 export class AppComponent {
 
+  isProduction = Environment.production;
+  secret = Environment.gapi.clientSecret;
+  clientId = Environment.gapi.clientId;
+
+  authService = inject(SocialAuthService);
+  http = inject(HttpClient);
+  accessToken: string | undefined;
+
+  async getAccessToken() {
+    if (!this.accessToken) {
+      return this.accessToken = await this.authService.getAccessToken(GoogleLoginProvider.PROVIDER_ID);
+    } else {
+      return this.accessToken;
+    }
+  }
+
+  async testGDrive2() {
+    await this.getAccessToken();
+    this.http.get('https://www.googleapis.com/drive/v3/files', {
+      headers: {Authorization: `Bearer ${this.accessToken}`}
+    }).subscribe(console.debug);
+  }
+
+  async testGDrive3() {
+    await this.getAccessToken();
+
+    this.http.get('https://www.googleapis.com/drive/v3/drives', {
+      headers: {Authorization: `Bearer ${this.accessToken}`},
+    }).subscribe(console.debug);
+
+    this.http.get('https://www.googleapis.com/drive/v3/files', {
+      headers: {Authorization: `Bearer ${this.accessToken}`},
+      params: {
+        //orderBy:"folder",
+
+        //corpora: "domain",
+        q: "'1JVVN4leTXQ46NsPZsPQgnmN0k5I7xaXx' in parents",
+        pageSize: 200
+      }
+    }).subscribe(console.debug);
+
+    this.http.get('https://www.googleapis.com/drive/v3/files', {
+      headers: {Authorization: `Bearer ${this.accessToken}`},
+      params: {
+        //orderBy:"folder",
+
+        //corpora: "domain",
+        q: "mimeType = 'application/vnd.google-apps.folder' and 'root' in parents",
+        pageSize: 200,
+        includeItemsFromAllDrives: true,
+        //supportsAllDrives: true
+      }
+    }).subscribe(console.debug);
+  }
+
+  async loadTextFile() {
+    const accessToken = await this.getAccessToken();
+    this.http.get('https://www.googleapis.com/drive/v3/files/1rqc0YifMrvjQtCkWILGjRLNaInQ2thnB', {
+      headers: {Authorization: `Bearer ${accessToken}`}, params: {alt:"media"}
+    }).subscribe((next:any) => {
+      console.debug(next);
+      console.debug(next.test)
+    });
+  }
+
+  async uploadTextFile() {
+    const accessToken = await this.getAccessToken();
+    const uploadUrl = 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart';
+    const fileName = 'test1.txt';
+    const fileContent = JSON.stringify({test: true}); // Replace with the actual file content
+
+    const metadata = {
+      name: fileName,
+      parents: ['root']
+    };
+
+    const headers = new HttpHeaders()
+      .set('Authorization', `Bearer ${accessToken}`);
+
+    const formData = new FormData();
+    formData.append('metadata', new Blob([JSON.stringify(metadata)], {type: 'application/json'}));
+    formData.append('file', new Blob([fileContent], {type: 'application/json'}));
+
+    this.http.post(uploadUrl, formData, {headers})
+      .subscribe(
+        (response) => {
+          console.log('File uploaded successfully:', response);
+        },
+        (error) => {
+          console.error('Error uploading file:', error);
+        }
+      );
+
+  }
 }
 
 export class Model {
-  healcubes: number = 0;
+  healcubes = 0;
   die: Die = Die.D20;
-  proficiency: number = 0;
-  public health: number = 0;
-  thealth: number = 0;
-  roll: number = 1;
+  proficiency = 0;
+  public health = 0;
+  thealth = 0;
+  roll = 1;
   rollarray: number[] = [];
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { SandboxComponent } from './components/sandbox/sandbox.component';
-import { FormsModule } from '@angular/forms';
-import { DndHistoryListComponent } from './components/dnd-history-list/dnd-history-list.component';
-import { CharSheetStatsComponent } from './components/char-sheet-stats/char-sheet-stats.component';
-import { ActionContainerComponent } from './components/action-container/action-container.component';
-import { ActionEntryComponent } from './components/action-entry/action-entry.component';
-import { DieRollerHealthComponent } from './die-roller-health/die-roller-health.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {SandboxComponent} from './components/sandbox/sandbox.component';
+import {FormsModule} from '@angular/forms';
+import {DndHistoryListComponent} from './components/dnd-history-list/dnd-history-list.component';
+import {CharSheetStatsComponent} from './components/char-sheet-stats/char-sheet-stats.component';
+import {ActionContainerComponent} from './components/action-container/action-container.component';
+import {ActionEntryComponent} from './components/action-entry/action-entry.component';
+import {DieRollerHealthComponent} from './die-roller-health/die-roller-health.component';
+import {
+  GoogleInitOptions,
+  GoogleLoginProvider,
+  SocialAuthServiceConfig,
+  SocialLoginModule
+} from "@abacritt/angularx-social-login";
+import {Environment} from "../environments/environment";
 
+export const googleLoginOptions: GoogleInitOptions = {
+  oneTapEnabled: true, // default is true
+  scopes: [
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/drive.file',
+    'https://www.googleapis.com/auth/drive.metadata'
+  ]
+};
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,9 +42,31 @@ import { DieRollerHealthComponent } from './die-roller-health/die-roller-health.
     AppRoutingModule,
     NgbModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    SocialLoginModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '473773108505-edl4nmrm6oakt1050fdaorbqmh5a4a51.apps.googleusercontent.com',
+              googleLoginOptions
+            )
+          }
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
